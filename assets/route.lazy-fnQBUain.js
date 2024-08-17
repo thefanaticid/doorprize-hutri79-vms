7d45aa0b46bcd2354e29761a371006981d98bb0b
fadhil mfriz <mfadhilrizki@budiluhur.ac.id>
2024-08-17 15:00:42 +0700

    Updates

--- assets/route.lazy-fnQBUain.js
+++ assets/route.lazy-fnQBUain.js
@@ -1,4 +1,4 @@
-import{m as r,b as a,L as o,j as e,o as t,B as n}from"./index-DKQfrRSx.js";/**
+import{m as r,b as a,L as o,j as e,o as t,B as n}from"./index-CpK2YR5i.js";/**
  * @license lucide-react v0.428.0 - ISC
  *
  * This source code is licensed under the ISC license.
